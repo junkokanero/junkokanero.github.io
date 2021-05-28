b7a4224fb377c56e26d3279325113ac66b34eeac
tutku-tnl <83888982+tutku-tnl@users.noreply.github.com>
2021-05-29 00:43:57 +0300

    Update exampleQIAT.js

--- IAT/exampleQIAT.js
+++ IAT/exampleQIAT.js
@@ -4,38 +4,39 @@ define(['pipAPI', 'https://junkokanero.github.io/IAT/quiat8.js'], function(APICo
 
 	return iatExtension({
 		category1 : {
-			name : 'Black people', //Will appear in the data.
+			name : 'Clothes', //Will appear in the data.
 			title : {
 				media : {word : 'Black people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    	//{image : 'black1.jpg'}, 
-			{image : 'axe_F_M_U.jpg'}, 
-    			{image : 'blender_M_F_U.jpg'}, 
-    			{image : 'bottle_F_N.jpg'}, 
-    			{image : 'bottleopener_M_N.jpg'}, 
-    			{image : 'cake_M_F_U.jpg'}, 
-    			{image : 'drill_F_M_U.jpg'}
+    		    	//
+			{image : 'bag_M_F_U_5_N.jpg'}, 
+			{image : 'belt_F_M_U.jpg'}, 
+    			{image : 'bonnet_M_N.jpg'}, 
+    			{image : 'bracelet_M_F_U_3.jpg'}, 
+    			{image : 'coat_M_M_M.jpg'}, 
+    			{image : 'eyeglass_F_N.jpg'}, 
+    			{image : 'glove_M_N.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'White people', //Will appear in the data.
+			name : 'Tools', //Will appear in the data.
 			title : {
 				media : {word : 'White people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'tweezers_F_F_M.jpg'}, 
-    			{image : 'tshirt_M_M_M.jpg'}, 
-    			{image : 'umbrella_M_N.jpg'}, 
-    			{image : 'wallet_M_M_M.jpg'}, 
-    			{image : 'watch_F_N.jpg'}, 
-    			{image : 'tie_F_M_U.jpg'}			], 
+    		    {image : 'hammer_M_M_M.jpg'}, 
+    			{image : 'jilet2.jpg'}, 
+    			{image : 'mixer_M_F_U.jpg'}, 
+    			{image : 'nailclipper_M_N.jpg'}, 
+    			{image : 'pliers_F_M_U.jpg'}, 
+    			{image : 'rollingpin_M_F_U.jpg'}			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
