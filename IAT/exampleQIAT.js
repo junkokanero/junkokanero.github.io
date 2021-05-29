3792a69d7450da68f311a837f5d383f2edc00ae1
tutku-tnl <83888982+tutku-tnl@users.noreply.github.com>
2021-05-29 22:04:56 +0300

    Update exampleQIAT.js

--- IAT/exampleQIAT.js
+++ IAT/exampleQIAT.js
@@ -18,6 +18,23 @@ define(['pipAPI', 'https://junkokanero.github.io/IAT/quiat8.js'], function(APICo
     			{image : 'bracelet_M_F_U_3.jpg'}, 
     			{image : 'coat_M_M_M.jpg'}, 
     			{image : 'eyeglass_F_N.jpg'}, 
+    			{image : 'glove_M_N.jpg'},
+			{image : 'hairclip_F_F_M.jpg'}, 
+			{image : 'hat_M_M_M.jpg'}, 
+    			{image : 'jacket_F_M_U_N.jpg'}, 
+    			{image : 'necktale_M_F_U_N.jpg'}, 
+    			{image : 'phonecase_F_N.jpg'}, 
+    			{image : 'ring_F_F_M_N.jpg'}, 
+    			{image : 'shirt_F_M_U.jpg'},
+			{image : 'skirt_F_F_M.jpg'}, 
+    			{image : 'sock_F_N.jpg'}, 
+    			{image : 'suit_M_M_M.jpg'}, 
+    			{image : 'sweater_M_N.jpg'}, 
+    			{image : 'swimsuit_M_F_U.jpg'},
+       			{image : 'tie_F_M_U.jpg'}, 
+    			{image : 'umbrella_M_N.jpg'}, 
+    			{image : 'wallet_M_M_M.jpg'}, 
+    			{image : 'watch_F_N.jpg'}, 
     			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
@@ -35,7 +52,7 @@ define(['pipAPI', 'https://junkokanero.github.io/IAT/quiat8.js'], function(APICo
     			{image : 'mixer_M_F_U.jpg'}, 
     			{image : 'nailclipper_M_N.jpg'}, 
     			{image : 'pliers_F_M_U.jpg'}, 
-    			{image : 'rollingpin_M_F_U.jpg'}	
+    			{image : 'rollingpin_M_F_U.jpg'},	
 			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
