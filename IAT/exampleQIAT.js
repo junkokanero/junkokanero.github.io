95363199faf7583051aef039d1d61ad79282832b
tutku-tnl <83888982+tutku-tnl@users.noreply.github.com>
2021-06-01 18:34:10 +0300

    Update exampleQIAT.js

--- IAT/exampleQIAT.js
+++ IAT/exampleQIAT.js
@@ -11,27 +11,30 @@ define(['pipAPI', 'https://junkokanero.github.io/IAT/quiat8.js'], function(APICo
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'bag_M_F_U_5_N.jpg'}, 
-				{image : 'belt_F_M_U.jpg'}, 
+    		   	{image : 'bag_M_F_U_5_N.jpg'}, 
+			{image : 'belt_F_M_U.jpg'}, 
     			{image : 'bonnet_M_N.jpg'}, 
     			{image : 'bracelet_M_F_U_3.jpg'}, 
     			{image : 'coat_M_M_M.jpg'}, 
     			{image : 'eyeglass_F_N.jpg'}, 
     			{image : 'glove_M_N.jpg'},
-				{image : 'hairclip_F_F_M.jpg'}, 
-				{image : 'hat_M_M_M.jpg'}, 
+			{image : 'hairclip_F_F_M.jpg'}, 
+			{image : 'hat_M_M_M.jpg'}, 
     			{image : 'jacket_F_M_U_N.jpg'}, 
     			{image : 'necktale_M_F_U_N.jpg'}, 
     			{image : 'phonecase_F_N.jpg'}, 
     			{image : 'ring_F_F_M_N.jpg'}, 
     			{image : 'shirt_F_M_U.jpg'},
-				{image : 'skirt_F_F_M.jpg'}, 
+			{image : 'skirt_F_F_M.jpg'}, 
     			{image : 'sock_F_N.jpg'}, 
     			{image : 'suit_M_M_M.jpg'}, 
     			{image : 'sweater_M_N.jpg'}, 
     			{image : 'swimsuit_M_F_U.jpg'},
        			{image : 'tie_F_M_U.jpg'}, 
     			{image : 'umbrella_M_N.jpg'}, 
+			{image : 'earring_F_F_M_2_N.jpg'},
+			{image : 'wallet_M_M_M.jpg'},
+			{image : 'watch_F_N.jpg'},
     			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
@@ -44,7 +47,7 @@ define(['pipAPI', 'https://junkokanero.github.io/IAT/quiat8.js'], function(APICo
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'axe_F_M_U.jpg'}, 
+    		    	{image : 'axe_F_M_U.jpg'}, 
     			{image : 'blender_M_F_U.jpg'}, 
     			{image : 'bottle_F_N.jpg'}, 
     			{image : 'bottleopener_M_N.jpg'}, 
@@ -66,6 +69,8 @@ define(['pipAPI', 'https://junkokanero.github.io/IAT/quiat8.js'], function(APICo
     			{image : 'tweezers_F_F_M.jpg'}, 
     			{image : 'woodenspoon_F_F_M.jpg'},
 			{image : 'wrench_F_M_U.jpg'}, 
+			{image : 'razor_M_M_M.jpg'},
+			{image : 'fork_F_N.jpg'},
 			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
