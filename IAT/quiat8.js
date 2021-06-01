07d87007597e7dab407fc0b461da504218094030
tutku-tnl <83888982+tutku-tnl@users.noreply.github.com>
2021-06-01 18:35:04 +0300

    Update quiat8.js

--- IAT/quiat8.js
+++ IAT/quiat8.js
@@ -123,9 +123,9 @@
 			////If you set the number of trials in any block to 0, that block will be skipped.
 			blockAttributes_nTrials : 20,
 			blockAttributes_nMiniBlocks : 5,
-			blockCategories_nTrials : 40,
+			blockCategories_nTrials : 48,
 			blockCategories_nMiniBlocks : 5,
-			blockFirstCombined_nTrials : 20,
+			blockFirstCombined_nTrials : 24,
 			blockFirstCombined_nMiniBlocks : 5,
 			blockSecondCombined_nTrials : 40, //Change to 0 if you want 5 blocks (you would probably want to increase blockFirstCombined_nTrials).
 			blockSecondCombined_nMiniBlocks : 21, 
