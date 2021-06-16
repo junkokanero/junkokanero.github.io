795e7c6582b9cd312d8a6fc93a221d1b44085bba
tutku-tnl <83888982+tutku-tnl@users.noreply.github.com>
2021-06-17 00:38:08 +0300

    Update quiat8.js

--- IAT/quiat8.js
+++ IAT/quiat8.js
@@ -210,9 +210,9 @@
 					'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' +
 					'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
 					'In this part, you will be asked to categorize the faces you see as “Female” or “Male”.<br/>' +
-					'Please put your left finger on E key and your right finger on I key. <br/>' +
-					'Press the <b>E</b> key for faces that belong to the category <font color="#0000ff">leftAttribute.</font>. <br/>' +
-					'<br/>Press the <b>I</b> key for faces that belong to the category <font color="#0000ff">rightAttribute</font>.<br/><br/>' +
+					'Please put your left finger on E key and your right finger on I key.<br/>' +
+					'Press the <b>E</b> key for faces that belong to the category <font color="#0000ff">leftAttribute.</font>.<br/>' +
+					'<br/>Press the <b>I</b> key for faces that belong to the category <font color="#0000ff">rightAttribute</font>.<br/>' +
 					'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
 					'Press the other key to continue.<br/>' +
 					'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>'+
