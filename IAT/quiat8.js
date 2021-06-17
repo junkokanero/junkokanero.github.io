834cc2772e0342510df863d3ddbeeb621731f57d
tutku-tnl <83888982+tutku-tnl@users.noreply.github.com>
2021-06-17 17:20:33 +0300

    Update quiat8.js

--- IAT/quiat8.js
+++ IAT/quiat8.js
@@ -300,10 +300,10 @@
 				instSecondCombined : '<div><p align="center" style="font-size:20px; font-family:arial">' +
 					'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' +
 					'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
-					'This is the same as the previous part.<br/>' +
-					'Use the <b>E</b> key for <font color="#336600">leftCategory</font> and for <font color="#0000ff">leftAttribute</font>.<br/>' +
-					'Use the <b>I</b> key for <font color="#336600">rightCategory</font> and for  <font color="#0000ff">rightAttribute</font>.<br/>' +
-					'Each item belongs to only one category.<br/><br/>' +
+					'<p align="center">This is the same as the previous part.<br/>' +
+					'<p align="center">Use the <b>E</b> key for <font color="#336600">leftCategory</font> and for <font color="#0000ff">leftAttribute</font>.<br/>' +
+					'<p align="center">Use the <b>I</b> key for <font color="#336600">rightCategory</font> and for  <font color="#0000ff">rightAttribute</font>.<br/>' +
+					'<p align="center">Each item belongs to only one category.<br/><br/>' +
 					'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
 					'Press the other key to continue.<br/>' + 
 					'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>' +
