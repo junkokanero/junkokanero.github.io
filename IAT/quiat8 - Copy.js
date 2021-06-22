8dfada21765187e3b6783e18c08991b0b66abe9d
tutku-tnl <83888982+tutku-tnl@users.noreply.github.com>
2021-06-22 21:23:07 +0300

    Update quiat8 - Copy.js

--- IAT/quiat8 - Copy.js
+++ IAT/quiat8 - Copy.js
@@ -140,15 +140,15 @@
 				////In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
 			////If you set the number of trials in any block to 0, that block will be skipped.
 			blockAttributes_nTrials : 36,
-			blockAttributes_nMiniBlocks : 2,
+			blockAttributes_nMiniBlocks : 1,
 			blockCategories_nTrials : 36,
-			blockCategories_nMiniBlocks : 2,
+			blockCategories_nMiniBlocks : 1,
 			blockFirstCombined_nTrials : 36,
-			blockFirstCombined_nMiniBlocks : 2,
+			blockFirstCombined_nMiniBlocks : 1,
 			blockSecondCombined_nTrials : 36, //Change to 0 if you want 5 blocks (you would probably want to increase blockFirstCombined_nTrials).
-			blockSecondCombined_nMiniBlocks : 2, 
+			blockSecondCombined_nMiniBlocks : 1, 
 			blockSwitch_nTrials : 36,
-			blockSwitch_nMiniBlocks : 2,
+			blockSwitch_nMiniBlocks : 1,
 	
 
 				//Should we randomize which attribute is on the right, and which on the left?
