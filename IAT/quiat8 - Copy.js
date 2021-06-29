09986c5bb8bdb2c02ac26889c4816143b7ef63bc
Junko Kanero <junkokanero@gmail.com>
2021-06-29 05:38:54 +0300

    Update quiat8 - Copy.js
    log header change

--- IAT/quiat8 - Copy.js
+++ IAT/quiat8 - Copy.js
@@ -411,7 +411,8 @@
 	            // Transform logs into a string
 	            // we save as CSV because qualtrics limits to 20K characters and this is more efficient.
 	            serialize: function (name, logs) {
-	                var headers = ['block', 'trial', 'cond', 'comp', 'type', 'cat',  'stim', 'resp', 'err', 'rt', 'd', 'fb', 'bOrd'];
+	                ////var headers = ['block', 'trial', 'cond', 'comp', 'type', 'cat',  'stim', 'resp', 'err', 'rt', 'd', 'fb', 'bOrd'];
+					var headers = ['block', 'trial', 'cond', 'comp', 'type', 'cat',  'stim', 'resp', 'err', 'rt', 'd', 'fb', 'bOrd','match'];
 	                //console.log(logs);
 	                var myLogs = [];
 	                var iLog;
