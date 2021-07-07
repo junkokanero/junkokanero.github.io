a33942fd954c6812b7de37a126266c01bfd11217
tutku-tnl <83888982+tutku-tnl@users.noreply.github.com>
2021-07-07 13:42:24 +0300

    Update quiat8 - Copy.js

--- IAT/quiat8 - Copy.js
+++ IAT/quiat8 - Copy.js
@@ -72,7 +72,7 @@
 				name : 'Kadın', //Will appear in the data.
 				title : {
 				media : {word : 'Kadın'}, //Name of the category presented in the task.
-				css : {color:'#0000FF','font-size':'2em'}, //Style of the category title.
+				css : {color:'#0000FF','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 					stimulusMedia : [ //Stimuli content as PIP's media objects
@@ -102,7 +102,7 @@
 				name : 'Erkek', //Will appear in the data.
 				title : {
 				media : {word : 'Erkek'}, //Name of the category presented in the task.
-				css : {color:'#0000FF','font-size':'2em'}, //Style of the category title.
+				css : {color:'#0000FF','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 					stimulusMedia : [ //Stimuli content as PIP's media objects
