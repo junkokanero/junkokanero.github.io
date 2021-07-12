b3a598e7060486978f1c2f3a53350318a8fe0593
Junko Kanero <junkokanero@gmail.com>
2021-07-12 12:55:01 +0300

    Update exampleQIAT - Copy.js
    Color change (correct)

--- IAT/exampleQIAT - Copy.js
+++ IAT/exampleQIAT - Copy.js
@@ -67,7 +67,7 @@ define(['pipAPI', 'https://junkokanero.github.io/IAT/quiat8 - Copy.js'], functio
 				name : 'Kadın', //Will appear in the data.
 				title : {
 				media : {word : 'Kadın'}, //Name of the category presented in the task.
-				css : {color:'#000ff','font-size':'2em'}, //Style of the category title.
+				css : {color:'#0000ff','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "or" in the combined block.
 			}, 
 					stimulusMedia : [ //Stimuli content as PIP's media objects
@@ -97,7 +97,7 @@ define(['pipAPI', 'https://junkokanero.github.io/IAT/quiat8 - Copy.js'], functio
 				name : 'Erkek', //Will appear in the data.
 				title : {
 				media : {word : 'Erkek'}, //Name of the category presented in the task.
-				css : {color:'#000ff','font-size':'2em'}, //Style of the category title.
+				css : {color:'#0000ff','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 					stimulusMedia : [ //Stimuli content as PIP's media objects
