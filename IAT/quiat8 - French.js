fdd2da1c1ed69bddc52c3c81e12e97b03b8c8a3e
Junko Kanero <junkokanero@gmail.com>
2022-04-19 15:10:16 +0300

    Update quiat8 - French.js

--- IAT/quiat8 - French.js
+++ IAT/quiat8 - French.js
@@ -229,7 +229,7 @@
 					'Veuillez appuyer sur la touche <b>E</b> pour les visages appartenant à la catégorie <b><font color="#0000ff">leftAttribute</font>.<br/></b>' +
 					'Veuillez appuyer sur la touche <b>I</b> pour les visages appartenant à la catégorie <b><font color="#0000ff">rightAttribute</font>.<br/></b>' +
 					'Veuillez répéter le mot ci-dessous à voix haute. Vous devrez continuer à répéter ce mot durant toutes la partie.<br/></b></b>' +
-					VERB1.bold() +
+					<p align="center">VERB1.bold()</p> +
 					'<u> Allez aussi loin que possible </u> en essayant d’être précis.<br/></p>'+
 					'<p align="center">Appuyez sur <b>la barre espace</b> quand vous êtes prêt à commencer.</font></p></div>',
 				instAttributePracticeTouch: [
